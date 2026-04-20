6b8afac9f82a149f826f26015a9480cafd2f4471
Huy-Hoang Dang <117700768+HoagnDawg35@users.noreply.github.com>
2026-04-20 21:00:33 +0700

    Add solution for sigmoid-numpy

---
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Vectorized sigmoid function.
+    """
+    x = np.asarray(x, dtype=float)
+
+    denominator = 1 + np.exp(-x)
+
+    return 1 / denominator
